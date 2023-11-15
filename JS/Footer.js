3fe1b37d35107d4c81a982a5d0eab940e5e8b954
RozbOnd <60060044+RozbOnd@users.noreply.github.com>
2023-11-15 21:10:08 +0300

    Update Footer.js

--- JS/Footer.js
+++ JS/Footer.js
@@ -5,7 +5,7 @@ document.getElementById('main').insertAdjacentHTML('afterend',
             <br>
             <a href="../Anketa.html">
                 <figure>
-                    <img src="../images/to-anketa.png" width="auto" 
+                    <img src="images/to-anketa.png" width="auto" 
                     height="100" border="0" alt="Пример">
                 <figcaption> Оставить отзыв</figcaption>
                 </figure>
