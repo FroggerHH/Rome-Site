ee7b7f35956004c9d1f737ba90d2b95c5b4ee125
Vyacheslav <slava.shilin07@mail.ru>
2023-11-15 20:05:33 +0300

    Update Footer.js

--- JS/Footer.js
+++ JS/Footer.js
@@ -2,5 +2,7 @@ document.getElementById('main').insertAdjacentHTML('afterend',
     `
         <footer>
             <p>&copy; 2023 Сайт о Римской Империи. Все права защищены.</p>
+            <br>
+            <a href="https://froggerhh.github.io/Rome-Site/Anketa.html">Оставить отзыв</a>
         </footer>
     `);
