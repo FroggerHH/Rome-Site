a25ccb54b24e876e7fbe81c502ddc6334af2e278
RozbOnd <60060044+RozbOnd@users.noreply.github.com>
2023-11-15 21:19:02 +0300

    Update Footer.js

--- JS/Footer.js
+++ JS/Footer.js
@@ -3,7 +3,7 @@ document.getElementById('main').insertAdjacentHTML('afterend',
         <footer>
             <p>&copy; 2023 Сайт о Римской Империи. Все права защищены.</p>
             <br>
-            <a href="../Anketa.html">
+            <a href="Anketa.html">
                 <figure>
                     <img src="images/to-anketa.png" width="auto" 
                     height="100" border="0" alt="Пример">
