4fa261def4e80dc15370836fc0dc87ff963126f4
Vyacheslav <slava.shilin07@mail.ru>
2023-11-16 21:27:12 +0300

    nothing

--- JS/ConnectStaff.js
+++ JS/ConnectStaff.js
@@ -3,7 +3,6 @@ const body = document.getElementsByTagName('body')[0];
 createScript('Hovering');
 createScript('Header');
 createScript('Footer');
-createScript('HtmlFromFile');
 
 
 function createScript(name) {
